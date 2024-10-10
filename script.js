cd115281e74147daa7d1e16e6b4519c6ddef30dc
Sudhanshu <160835970+Sudhanshu517@users.noreply.github.com>
2024-10-10 19:26:20 +0530

    Add files via upload
    MY PORTFOLIO WEBSITE

---
+++ script.js
@@ -0,0 +1,13 @@
+var typed = new Typed('#element', {
+    strings: ['Your Partner in Digital Transformation!'],
+    typeSpeed: 30,
+  });
+
+  var sidemenu = document.getElementById("sidemenu")
+
+  function openmenu(){
+    sidemenu.style.right = "0";
+  }
+  function closemenu(){
+    sidemenu.style.right = "-300px";
+  }
